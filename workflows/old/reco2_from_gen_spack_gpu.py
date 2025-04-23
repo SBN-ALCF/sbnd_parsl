f5917fb84522ca10612af636c525738f63fd3d44
tbwester <thomas_w@live.com>
2025-04-23 18:37:59 +0000

    deprecate _gpu workflow variation

--- workflows/old/reco2_from_gen_spack_gpu.py
+++ workflows/old/reco2_from_gen_spack_gpu.py
@@ -15,27 +15,36 @@ from parsl.app.app import bash_app
 
 from sbnd_parsl.workflow import StageType, Stage, Workflow, WorkflowExecutor
 from sbnd_parsl.metadata import MetadataGenerator
-from sbnd_parsl.templates import CMD_TEMPLATE_SPACK
+from sbnd_parsl.templates import SINGLE_FCL_TEMPLATE, CAF_TEMPLATE_SPACK, \
+    SINGLE_FCL_TEMPLATE_SPACK
 from sbnd_parsl.utils import create_default_useropts, create_parsl_config, \
-    hash_name, subrun_dir
+    hash_name
 
-
-def fcl_future(workdir, stdout, stderr, template, cmd, larsoft_opts, inputs=[], outputs=[], pre_job_hook='', post_job_hook=''):
+@bash_app(cache=True)
+def fcl_future(workdir, stdout, stderr, template, larsoft_opts, inputs=[], outputs=[], pre_job_hook='', post_job_hook='', parsl_resource_specification={}):
     """Return formatted bash script which produces each future when executed."""
     return template.format(
         fhicl=inputs[0],
         workdir=workdir,
         output=outputs[0],
         input=inputs[1],
-        cmd=cmd,
         **larsoft_opts,
         pre_job_hook=pre_job_hook,
         post_job_hook=post_job_hook,
     )
 
 
-def runfunc(self, fcl, inputs, run_dir, executor):
+def runfunc(self, fcl, input_files, run_dir, executor):
     """Method bound to each Stage object and run during workflow execution."""
+
+    fcl_fullpath = executor.fcl_dir / fcl
+    inputs = [str(fcl_fullpath), None]
+    if input_files is not None:
+        inputs = [str(fcl_fullpath)] + input_files
+
+    run_dir.mkdir(parents=True, exist_ok=True)
+    output_dir = executor.output_dir / self.stage_type.value
+    output_dir.mkdir(parents=True, exist_ok=True)
 
     output_filename = ''.join([
         str(self.stage_type.value), '-',
@@ -44,18 +53,7 @@ def runfunc(self, fcl, inputs, run_dir, executor):
     ])
     executor.lar_run_counter += 1
 
-    output_dir = executor.output_dir / self.stage_type.value
-    if self.combine:
-        # if we are combining, save this stage's result only on node-local disk
-        # output_dir.name gets the instance number for this task
-        run_dir = pathlib.Path('/local/scratch') / run_dir.name
-        output_dir = run_dir
-    else:
-        # save this result to filesystem (eagle). Make sure it exists
-        output_dir.mkdir(parents=True, exist_ok=True)
-
-    output_file = output_dir / output_filename
-    output_file_arg_str = f'--output {str(output_file)}'
+    output_filepath = output_dir / output_filename
     # mg_cmd = executor.meta.run_cmd(
     #     output_filename + '.json', os.path.basename(fcl), check_exists=False)
 
@@ -67,49 +65,62 @@ def runfunc(self, fcl, inputs, run_dir, executor):
         #     f'echo "source.firstRun: {run_number}" >> {os.path.basename(fcl)}',
         #     f'echo "source.firstSubRun: {subrun_number}" >> {os.path.basename(fcl)}'
         # ])
-    
-    input_file_arg_str = ''
-    parent_cmd = ''
-    input_arg = [str(fcl), None]
-    if inputs is not None:
-        input_files = inputs[0]
-        parent_cmd = inputs[1]
-        input_file_arg_str = \
-            ' '.join([f'-s {str(file)}' if not isinstance(file, parsl.app.futures.DataFuture) else f'-s {str(file.filepath)}' for file in input_files])
-        input_arg = [str(fcl)] + [str(f) if not isinstance(f, parsl.app.futures.DataFuture) else f for f in input_files]
 
-    cmd = f'mkdir -p {run_dir} && cd {run_dir} && lar -c {fcl} {input_file_arg_str} {output_file_arg_str}'
+    task_parsl_resource_specification = {'cores': 1, 'memory': 100, 'disk': 500, 'priority':10}
+    if self.stage_type == StageType.DETSIM:
+        task_parsl_resource_specification = {'cores': 1, 'memory': 100, 'disk': 500, 'gpus': 1, 'priority':100}
 
-    if self.stage_type == StageType.GEN:
-        cmd += f' --nevts {executor.larsoft_opts["nevts"]}'
-
-    if parent_cmd != '':
-        cmd = ' && '.join([parent_cmd, cmd])
-
-    if self.combine:
-        # don't submit work, just forward commands to the next task
-        return [[output_file], cmd]
-
-    # submit cmd as a parsl bash_app
-    future_func = functools.partial(fcl_future)
-    future_func.__name__ = self.stage_type.value
-    app = bash_app(future_func, cache=True)
-
-    future = app(
+    future = fcl_future(
         workdir = str(run_dir),
         stdout = str(run_dir / output_filename.replace(".root", ".out")),
         stderr = str(run_dir / output_filename.replace(".root", ".err")),
-        template = CMD_TEMPLATE_SPACK,
-        cmd=cmd,
+        template = SINGLE_FCL_TEMPLATE_SPACK,
         larsoft_opts = executor.larsoft_opts,
-        inputs = input_arg,
-        outputs = [File(str(output_file))],
+        inputs = inputs,
+        outputs = [File(str(output_filepath))],
+        parsl_resource_specification = task_parsl_resource_specification
+    )
+
+    # this modifies the list passed in by WorkflowExecutor
+    executor.futures.append(future.outputs[0])
+    return future.outputs
+
+
+def runfunc_caf(self, fcl, input_files, run_dir, executor):
+    """Method bound to each Stage object and run during workflow execution."""
+
+    fcl_fullpath = executor.fcl_dir / fcl
+    caf_input_arg = ' '.join([f'{fname.filename}' for fname in input_files])
+    inputs = [str(fcl_fullpath), caf_input_arg] + input_files
+
+    run_dir.mkdir(parents=True, exist_ok=True)
+    output_dir = executor.output_dir / self.stage_type.value
+    output_dir.mkdir(parents=True, exist_ok=True)
+
+    output_filename = os.path.splitext(os.path.basename(input_files[0].filename))[0] + '.flat.caf.root'
+    executor.lar_run_counter += 1
+
+    output_filepath = output_dir / output_filename
+    mg_cmd = ''
+    # mg_cmd = executor.meta.run_cmd(
+    #     output_filename + '.json', os.path.basename(fcl), check_exists=False)
+
+    future = fcl_future(
+        workdir = str(run_dir),
+        stdout = str(run_dir / output_filename.replace(".root", ".out")),
+        stderr = str(run_dir / output_filename.replace(".root", ".err")),
+        template = CAF_TEMPLATE_SPACK,
+        larsoft_opts = executor.larsoft_opts,
+        inputs = inputs,
+        outputs = [File(output_filepath)],
+        pre_job_hook = mg_cmd,
+        parsl_resource_specification = {'cores': 1, 'memory': 100, 'disk': 500, 'priority': 10}
     )
 
     # this modifies the list passed in by WorkflowExecutor
     executor.futures.append(future.outputs[0])
 
-    return [future.outputs, '']
+    return future.outputs
 
 
 class Reco2FromGenExecutor(WorkflowExecutor):
@@ -127,34 +138,44 @@ class Reco2FromGenExecutor(WorkflowExecutor):
     def setup_single_workflow(self, iteration: int):
         workflow = Workflow(self.stage_order, default_fcls=self.fcls)
         runfunc_ = functools.partial(runfunc, executor=self)
+        runfunc_caf_ = functools.partial(runfunc_caf, executor=self)
         s = Stage(StageType.CAF)
-        s.run_dir = subrun_dir(self.output_dir, iteration)
-        s.runfunc = runfunc_
+        s.run_dir = get_subrun_dir(self.output_dir, iteration)
         workflow.add_final_stage(s)
+        s.runfunc = runfunc_caf_
 
+        workflow.add_final_stage(s)
         for i in range(self.subruns_per_caf):
             inst = iteration * self.subruns_per_caf + i
             # create reco2 file from MC, only need to specify the last stage
             # since there are no inputs
             s2 = Stage(StageType.RECO2)
+            s2.runfunc = runfunc_
 
             # each reco2 file will have its own directory
-            s2.run_dir = subrun_dir(self.output_dir, inst)
-            s.add_parents(s2, self.fcls)
+            s2.run_dir = get_subrun_dir(self.output_dir, inst)
+            s.add_parents(s2, workflow.default_fcls)
 
         return workflow
 
 
+def get_subrun_dir(prefix: pathlib.Path, subrun: int):
+    """Returns a path with directory structure like XXXX00/XXXXXX"""
+    return prefix / f"{100*(subrun//100):06d}" / f"subrun_{subrun:06d}"
+
+
 def main(settings):
+    # parsl
     user_opts = create_default_useropts()
     user_opts['run_dir'] = str(pathlib.Path(settings['run']['output']) / 'runinfo')
     user_opts.update(settings['queue'])
-    parsl_config = create_parsl_config(user_opts, [settings['larsoft']['spack_top'], settings['larsoft']['version']])
+    parsl_config = create_parsl_config(user_opts, [settings['larsoft']['spack_top'],settings['larsoft']['version']])
     print(parsl_config)
     parsl.clear()
-    with parsl.load(parsl_config):
-        wfe = Reco2FromGenExecutor(settings)
-        wfe.execute()
+    parsl.load(parsl_config)
+
+    wfe = Reco2FromGenExecutor(settings)
+    wfe.execute()
 
 
 if __name__ == '__main__':
